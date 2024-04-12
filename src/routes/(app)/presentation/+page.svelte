3fd3d1658bc36a8bef8ee4cee276248ebc0089d9
tuturmann <mmanon.leclercq@gmail.com>
2024-04-12 11:19:47 +0200

    update

--- src/routes/(app)/presentation/+page.svelte
+++ src/routes/(app)/presentation/+page.svelte
@@ -23,8 +23,8 @@
     
     <img class="separator" src="line.png" alt="separator">
 
-    <span>Je suis Manon, { calculateMyAge() } ans, étudiante en deuxième année de BUT Informatique à Lille.</span>
-    <span>Je recherche un stage en développement d’applications, développement web ou bases de données du <strong>15 avril au 21 juin 2024</strong>, avec si possible <strong>poursuite en alternance</strong>.</span>
+    <span>Je suis Manon Leclercq, { calculateMyAge() } ans, étudiante en deuxième année de BUT Informatique à l'IUT A de Lille.</span>
+    <span></span>
 </div>
 
 <img class="drawing" src="home-portfolio.png" alt="background drawing">
